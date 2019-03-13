af21d6723478f96463a15ebba129b8f66873b324
Happeace304 <happeace304@gmail.com>
2019-03-13 11:13:20 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,12 +13,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesTableSeeder::class); //tạo Role mặc định
         $this->call(UserRolesTableSeeder::class); //tự động tạo USER và thêm USERROLE
-<<<<<<< HEAD
         $this->call(ProductCategorySeeder::class);
         $this->call(ProductSeeder::class);
-        $this->call(ProductOptionSeeder::class);     
-=======
+        $this->call(ProductOptionSeeder::class);
         $this->call(AdminSeeder::class);
->>>>>>> 805422e2a284abe42b115f2c79050e6dba37dafd
     }
 }
